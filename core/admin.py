feb1cdf92f9a47592f7dabf754ba2c3d386de753
saintzema <emezeji98@gmail.com>
2026-07-31 17:26:31 +0100

    Add ContactInfo model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ContactInfo
 
-# Register your models here.
+
+@admin.register(ContactInfo)
+class ContactInfoAdmin(admin.ModelAdmin):
+    list_display = (
+        "company_name",
+        "phone",
+        "email",
+        "updated_at",
+    )
--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactInfo(models.Model):
+    company_name = models.CharField(max_length=100, default="B Square Telecom")
+    phone = models.CharField(max_length=20)
+    whatsapp = models.CharField(max_length=20, blank=True)
+    email = models.EmailField()
+    address = models.TextField(blank=True)
+
+    facebook = models.URLField(blank=True)
+    instagram = models.URLField(blank=True)
+    telegram = models.URLField(blank=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.company_name
