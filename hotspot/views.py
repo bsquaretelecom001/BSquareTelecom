18ebb82a5c384b4b0b482c4fd5ed466de0c6a297
saintzema <emezeji98@gmail.com>
2026-09-13 20:58:38 +0100

    Add Omada captive portal client parameters

--- hotspot/views.py
+++ hotspot/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def portal(request):
+    context = {
+        "client_mac": request.GET.get("clientMac", ""),
+        "client_ip": request.GET.get("clientIp", ""),
+        "ap_mac": request.GET.get("apMac", ""),
+        "ssid_name": request.GET.get("ssidName", ""),
+        "radio_id": request.GET.get("radioId", ""),
+        "site": request.GET.get("site", ""),
+        "redirect_url": request.GET.get("redirectUrl", ""),
+    }
+
+    return render(
+        request,
+        "hotspot/portal.html",
+        context,
+    )
